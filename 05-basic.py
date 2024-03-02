d15a42f2f73faecd93af9ce8f4b19948ac449fbb
Jgarette0 <aajiliangarette@gmail.com>
2024-03-03 07:31:25 +0800

    int function and input function

---
+++ 05-basic.py
@@ -0,0 +1,13 @@
+# salute  = input("salutations:")
+# name = input("Yout name:");
+# print("hello "+salute +" " + name)
+
+def addNum(first, second):
+ total = first + second
+ return (total)
+
+first = int(input("Input first number:"))
+second = int(input("Input second number"))
+
+
+print("The total sum is equals to " + str(addNum(first,second)))
