b69773137e5cf9c36075b5dc6d1f13fa05d5b34e
Jgarette0 <aajiliangarette@gmail.com>
2024-03-04 06:08:18 +0800

    for loops, break,continue, while loop,range and nested for loop

---
+++ 11-basic.py
@@ -0,0 +1,36 @@
+#Loop in python
+
+cars = ["supra", "gtr", "formula1", "ferrari"]
+for brand in cars:
+ if(brand == "formula1"):
+  continue
+ print(brand)
+ 
+ 
+ 
+cars = ["supra", "gtr", "formula1", "ferrari"]
+for brand in cars:
+ if(brand == "formula1"):
+  break
+ print(brand)
+ 
+ car = range(5)
+ for nth in car:
+  print(nth)
+  
+
+name = ["jilian", "jacob", "malaria", "jonabels"]
+age = [21,13,24,20]
+
+for ngalan in name:
+ for edad in age:
+  print(ngalan,edad)
+  
+  
+  i = 1
+  while i <= 10:
+   i += 1
+   if i == 3:
+    continue
+   print(i)
+   
